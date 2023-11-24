3d1804deb292151fac4e43bd2393491edf67850c
hashansl <hashan.dan@gmail.com>
2023-11-24 18:14:27 -0500

    testing

--- washington dc/rcode/washingtondc.R
+++ washington dc/rcode/washingtondc.R
@@ -1,1 +1,11 @@
 library(sf)
+library(mgcv)
+
+dcRaw <- st_read("~/MacBook/git/opioid-risk-index/washington dc/data/processed data/SVI2020 WashingtonDC counties with death rate/SVI2020_WashingtonDC_counties_with_death_rate.shp")
+
+
+# Using names() function
+column_names <- names(dcRaw)
+print(column_names)
+
+print("yes")
